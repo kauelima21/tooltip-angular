ff4243c221500131af3e5277187baf41f5cf745c
kauelima21 <kaueslim@outlook.com>
2023-01-16 09:02:55 -0300

    send to remote repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TooltipComponent } from './components/tooltip/tooltip.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TooltipComponent
   ],
   imports: [
     BrowserModule
