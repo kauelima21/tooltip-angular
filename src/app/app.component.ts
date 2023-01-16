6056d812de0f5cb0c497386d07be085e0ef3a379
kauelima21 <kaueslim@outlook.com>
2023-01-16 16:18:46 -0300

    usign select and content projection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'tooltip';
+  show: boolean = false;
+
+  toggleModal() {
+    this.show = !this.show;
+  }
 }
